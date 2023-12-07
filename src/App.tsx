3dda990f95d88d80213eedf08cfb36fb6dc8eb6e
Andrew Westgerdes <ajwestgerdes@gmail.com>
2023-12-07 00:00:07 -0600

    scrollable columns, layout stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Nav from './components/Nav'
 import './App.css'
 import TaskView from './views/TaskView'
+import { Grid } from '@mui/material'
 
 function App() {
 
   return (
     <>
-    <Nav/>
-    <TaskView/>
+    <Grid container>
+      <Nav/>
+      <TaskView/>
+    </Grid>
     </>
   )
 }
--- src/views/TaskView.tsx
+++ src/views/TaskView.tsx
@@ -1,7 +1,44 @@
-import TaskColumn from "../components/TaskColumn";
+import { Divider, Grid, Box, Typography } from '@mui/material';
+import Task from '../components/Task';
 
 export default function TaskView() {
   return (
-   <TaskColumn />
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={4} padding={20}>
+          <Grid>
+            <Typography variant='h5' maxWidth={345}>
+              test column
+            </Typography>
+              <Box style={{maxHeight: '100vh', overflow: 'auto'}}>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+                <Task/>
+              </Box>
+          </Grid>
+          
+        
+        <Divider orientation="vertical" />
+        <Grid>
+            <Task/>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
