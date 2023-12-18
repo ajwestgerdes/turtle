5de69219b3889075fbc28392ac657f23d66fec2e
Andrew Westgerdes <ajwestgerdes@gmail.com>
2023-12-17 22:15:20 -0600

    some clean up/formatting. Added todos

--- src/views/TaskView.tsx
+++ src/views/TaskView.tsx
@@ -21,25 +21,26 @@ interface Task {
 
 export default function TaskView() {
   const data = dummyData as Task
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container mt={15} px={20}>
-            {data.columnOrder.map(colId => {
-              return (
-                <Grid pr={10} minWidth={345}>
-                  <Typography variant='h5'align='center' >
-                    {data.columns[colId].title}
-                  </Typography>
-                  <Box style={{maxHeight: '100vh', overflow: 'auto'}}>
-                    {data.columns[colId].taskIds.map(taskId => {
-                      return (
-                        <Task taskId={data.tasks[taskId].id} content={data.tasks[taskId].content} />
-                      )
-                    })}
-                  </Box>
-                </Grid>
-              )   
-            })}     
+        {data.columnOrder.map(colId => {
+          return (
+            <Grid pr={10} minWidth={345}>
+              <Typography variant='h5'align='center' >
+                {data.columns[colId].title}
+              </Typography>
+              <Box style={{maxHeight: '100vh', overflow: 'auto'}}>
+                {data.columns[colId].taskIds.map(taskId => {
+                  return (
+                    <Task taskId={data.tasks[taskId].id} content={data.tasks[taskId].content} />
+                  )
+                })}
+              </Box>
+            </Grid>
+          )   
+        })}     
         <Divider orientation="vertical" />
       </Grid>
     </Box>
